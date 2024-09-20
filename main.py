4f95f31702fa1664dc6146823229d971584627d8
Gustavo Pessanha <gupessanha@poli.ufrj.br>
2024-09-20 10:56:59 -0300

    adiciona classe Graph e funcionalidade para carregar nós e arestas a partir de um arquivo

--- main.py
+++ main.py
@@ -11,14 +11,39 @@ class Node:
         for i in self.adj:
             l.append(i.value)
         return str(self.value) + " -> " + str(l)
+
+class Graph:
+    def __init__(self, nodes = []):
+        self.nodes = nodes
+        
+    def add_node(self, node):
+        self.nodes.append(node)
+        
+    def add_edge(self, node1, node2):
+        node1.add_adj(node2)
+        node2.add_adj(node1)
     
-A = Node("A")
-B = Node("B")
-C = Node("C")
-D = Node("D")
-E = Node("E")
-
-A.add_adj(B)
-A.add_adj(C)
-
-print(A)
+    def load_from_file(self, file):
+        f = open(file, "r")
+        for line in f:
+            line = line.strip().split(" ")
+            
+            if len(line) != 2:
+                continue
+            else:
+                n1 = Node(line[0])
+                n2 = Node(line[1])
+                self.add_node(n1)
+                self.add_node(n2)
+                self.add_edge(n1, n2)
+        f.close()
+        
+    def __str__(self):
+        s = ""
+        for i in self.nodes:
+            s += str(i) + "\n"
+        return s
+    
+grafo = Graph()
+grafo.load_from_file("example_input.txt")
+print(grafo)
